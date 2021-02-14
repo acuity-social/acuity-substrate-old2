ef4752acf11489a22e70842fc2a60ed3ee5f4bbd
Jonathan Brown <jbrown@acuity.social>
2021-02-14 11:05:13 +0700

    Increment spec_version

--- runtime/acuity/src/lib.rs
+++ runtime/acuity/src/lib.rs
@@ -90,7 +90,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("acuity"),
 	impl_name: create_runtime_str!("acuity-substrate"),
 	authoring_version: 1,
-	spec_version: 3,
+	spec_version: 4,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
